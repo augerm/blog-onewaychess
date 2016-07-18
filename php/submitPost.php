73c5a722a063c0cb60812080484cc25380d5fb3f
Emre Anamur <Emre Anamur>
2016-07-17 20:56:45 -0400

    changing bariables

--- php/submitPost.php
+++ php/submitPost.php
@@ -11,9 +11,9 @@ $date = date("n-j-Y g:i a");
 	if(strlen($entry) >= 1) {
 		$server = "us-cdbr-iron-east-04.cleardb.net";
         $username = "b0c4b9423d2803";
-        $password = "48a9e62a";
+        $password2 = "48a9e62a";
         $db = "heroku_1dd2b8ffb0f1998";
-        $con = new mysqli($server, $username, $password, $db);
+        $con = new mysqli($server, $username, $password2, $db);
         if ($con->connect_error) {
             die("Connection failed: " . $con->connect_error);
         }
@@ -25,17 +25,11 @@ $date = date("n-j-Y g:i a");
 		$topic = "'" . $topic . "'";
 		$date = "'" . $date . "'";
 		$password = hash('ripemd160', $password);
-		echo "password hashed: ";
-		echo hash('ripemd160', 'password');
 		//$sqlTest = "SELECT password from posters where $userName = (select email from posters)";
 		$sqlTest = "SELECT password from posters where $userName = posters.email";		
 		$result = $con->query($sqlTest);
 		$row = $result->fetch_assoc();
 		if ($row["password"] != $password) {
-			echo "our password hashed: ";
-			echo $row["password"];
-			echo "given password hashed: ";
-			echo $password;
 			die("invalid login information");
 		}
 		$sql = "INSERT INTO entries (mydate, title, entryText, topic, pEmail) VALUES ($date,$title,$entry,$topic,$userName)";
@@ -54,4 +48,7 @@ $date = date("n-j-Y g:i a");
 
 	$con->close();
 ?> 
-2c08e8f5884750a7b99f6f2f342fc638db25ff312c08e8f5884750a7b99f6f2f342fc638db25ff315bead86e6b60f15d36e9ca2fb0bbd5f9b8dc0ab5invalid login information
+password hashed: 2c08e8f5884750a7b99f6f2f342fc638db25ff31
+our password hashed: 2c08e8f5884750a7b99f6f2f342fc638db25ff31
+given password hashed: 5bead86e6b60f15d36e9ca2fb0bbd5f9b8dc0ab5
+invalid login information
